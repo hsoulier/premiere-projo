76ba64239943821024b3c7f566c501d0296ac806
hsoulier <hi.soulier@gmail.com>
2025-07-01 08:30:12 +0200

    🔨 Feat: Add Louxor cinema scraper

--- scripts/scrapper.js
+++ scripts/scrapper.js
@@ -3,22 +3,27 @@ import { scrapUGC } from "./providers/ugc.js"
 import { scrapPathe } from "./providers/pathe2.js"
 import { scrapMk2 } from "./providers/mk2.js"
 import { scrapGrandRex } from "./providers/grand-rex.js"
+import { scrapLouxor } from "./providers/louxor.js"
 
 const init = async () => {
-  console.group("🛠️ scraping Pathé")
-  await scrapPathe()
-  console.groupEnd()
+  // console.group("🛠️ scraping Pathé")
+  // await scrapPathe()
+  // console.groupEnd()
 
-  console.group("🛠️ scraping Mk2")
-  await scrapMk2()
-  console.groupEnd()
+  // console.group("🛠️ scraping Mk2")
+  // await scrapMk2()
+  // console.groupEnd()
 
-  console.group("🛠️ scraping UGC")
-  await scrapUGC()
-  console.groupEnd()
+  // console.group("🛠️ scraping UGC")
+  // await scrapUGC()
+  // console.groupEnd()
 
-  console.group("🛠️ scraping Le Grand Rex")
-  await scrapGrandRex()
+  // console.group("🛠️ scraping Le Grand Rex")
+  // await scrapGrandRex()
+  // console.groupEnd()
+
+  console.group("🛠️ scraping Louxor")
+  await scrapLouxor()
   console.groupEnd()
 
   await sql.end()
